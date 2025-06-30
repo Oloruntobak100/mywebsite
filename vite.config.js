3e15ddbb58f5dd4882263effed702550c4660b62
unknown <kaytoba49@gmail.com>
2025-06-30 14:18:41 +0100

    Updated vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/mywebsite/', // Add this line - must match your GitHub repo name
   server: {
     port: 5174,
   },
